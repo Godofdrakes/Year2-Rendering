d5489e7243e0420acf45853133dba5271531cd26
Urias Rooney <Godofdrakes@gmail.com>
2015-09-10 11:52:45 -0700

    Added example main to Application header.

--- depend/include/Year2-Rendering/Application.h
+++ depend/include/Year2-Rendering/Application.h
@@ -16,6 +16,22 @@ enum ApplicationFail {
     OGL_LOAD_FUNCTIONS,
 };
 
+/* Example loop
+
+int main() {
+    Application* app = new Application();
+    if ( app->Init() == ApplicationFail::NONE ) {
+        while ( app->Tick() ) {
+            app->Update();
+            app->Render();
+        }
+        app->Shutdown();
+    }
+    return 0;
+}
+
+*/
+
 class Application {
     string name_string_;
     int width_i_, height_i_;
